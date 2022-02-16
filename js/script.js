721e82b07fc089e36cf3e306f640aca978e71919
manic929 <64918035+manic929@users.noreply.github.com>
2022-02-16 10:38:43 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+$(".dws-form").on("click", ".tab", function (){
+$(".dws-form").find(".active").removeClass("active");
+$(this).addClass('active');
+$(".tab-form").eq($(this).index()).addClass("active");
+});
